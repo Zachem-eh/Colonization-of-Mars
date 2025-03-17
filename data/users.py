1bfa292977fadfa44addc79f6da19598cf3607a5
Zachem-eh <vladsasha7002@gmail.com>
2025-03-17 18:44:04 +0300

    mega update

--- data/users.py
+++ data/users.py
@@ -1,10 +1,13 @@
 import sqlalchemy
 import datetime
 import sqlalchemy.orm as orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
@@ -17,3 +20,10 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False, default=datetime.datetime.now)
     jobs = orm.relationship('Jobs', back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return self.check_password == password
+        # return check_password_hash(self.hashed_password, password)
